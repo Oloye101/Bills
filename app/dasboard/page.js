21b6838101af718e0931ea10bd628a15fe1808e5
Onyebuchi Chibuike <101michaelchibuike@gmail.com>
2024-10-09 13:49:16 +0100

    Updated the UI dashboad

--- app/dasboard/page.js
+++ app/dasboard/page.js
@@ -4,6 +4,9 @@ export default function Dashboard (){
     return (
         <main>
             <p> User dashboad</p>
+            <p> Propose to UI design</p>
+            <p>Build the UI</p>
+            <p>Propose possible changes</p>
         </main>
     )
 }
